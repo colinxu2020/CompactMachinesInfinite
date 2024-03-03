9f6b16b3cb3806d9a0131800e23f4c8ba0087b09
Colinxu2020 <colinxu2020@gmail.com>
2024-03-03 17:56:13 +0800

    update to fg6, start dev of 1.20.4

--- src/main/java/top/fireddev/compactmachinesinfinite/CompactMachinesInfinite.java
+++ src/main/java/top/fireddev/compactmachinesinfinite/CompactMachinesInfinite.java
@@ -1,9 +1,17 @@
 package top.fireddev.compactmachinesinfinite;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,6 +22,7 @@ public class CompactMachinesInfinite
     // Define mod id in a common place for everything to reference
     public static final String MODID = "compactmachinesinfinite";
     public static final Logger LOGGER = LogManager.getLogger();
+
     public CompactMachinesInfinite()
     {
         ModLoadingContext ctx = ModLoadingContext.get();
